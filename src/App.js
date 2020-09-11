5fe9a1efdf194f040d887d378f0fb8fcd95ac529
taitd153 <trinhdinhtai1996@gmail.com>
2020-09-11 20:52:52 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
+
 function App() {
   return (
     <div className="App">
       {/* Header */}
       <Header />
       {/* Home */}
+      <Home />
     </div>
   );
 }
