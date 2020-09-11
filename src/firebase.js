61f5a304db5ac86d9a15dc8b42e92a114e0ae628
taitd153 <trinhdinhtai1996@gmail.com>
2020-09-11 17:14:41 +0700

    commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBvf817LMy30XgQM6N7Vv_6tYFWo5SLNLU",
+    authDomain: "clone-e93c8.firebaseapp.com",
+    databaseURL: "https://clone-e93c8.firebaseio.com",
+    projectId: "clone-e93c8",
+    storageBucket: "clone-e93c8.appspot.com",
+    messagingSenderId: "543523702229",
+    appId: "1:543523702229:web:228e4110218d79937e695c",
+    measurementId: "G-TMNDLRZ51T"
+  };
